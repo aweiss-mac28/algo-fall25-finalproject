7c1a780a53f1624a668489a34663afc8c333e6eb
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2025-12-03 19:36:25 -0600

    Old Mastermind Files

---
+++ Sequence.java
@@ -0,0 +1,11 @@
+package mastermind;
+
+import java.util.ArrayList;
+
+import edu.macalester.graphics.GraphicsGroup;
+
+public interface Sequence {
+    public ArrayList<CodePin> getSequence();
+    public String getSequenceAsString();
+    public GraphicsGroup getSequenceGraphics();
+}
